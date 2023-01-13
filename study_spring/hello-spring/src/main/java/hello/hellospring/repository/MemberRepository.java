6c2c595929c03e8fe045831fbb59735fb716e2bc
CJiu01 <jiwoo020801@gmail.com>
2023-01-13 23:12:26 +0900

    Member 레포지토리 생성

---
+++ study_spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member); //Member 저장한 뒤, Member 반환
+    Optional<Member> findByID(Long id); //Null 반환 시, Optional에 감싸 반환함.
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
