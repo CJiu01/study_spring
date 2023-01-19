3fcb4304da575afc91017c165ec442d84e51b1b9
최지우 <cjw@choejiuui-MacBookPro.local>
2023-01-19 19:38:58 +0900

    GET 메서드 구현

---
+++ study_spring/api/src/main/java/com/springboot/api/Controller/GetController.java
@@ -0,0 +1,42 @@
+package com.springboot.api.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        return "Hello World";
+    }
+
+    @GetMapping(value = "/name")
+    public String getName() {
+        return "Flature";
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(@RequestParam String name, @RequestParam String email, @RequestParam String organization) {
+        return name + " " + email + " " + organization;
+    }
+
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+
+    }
+}
