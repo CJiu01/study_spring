4c9132354995ea36ae75f1cf00514fa341d64796
최지우 <cjw@choejiuui-MacBookPro.local>
2023-01-19 17:10:09 +0900

    first commit

---
+++ study_spring/bok1/src/main/java/com/example/bok1/Bok1Application.java
@@ -0,0 +1,13 @@
+package com.example.bok1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bok1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bok1Application.class, args);
+	}
+
+}
