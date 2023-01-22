ece146ae699c4d1010da1e3dc7138fc03ba28158
최지우 <cjw@choejiuui-MacBookPro.local>
2023-01-23 00:28:37 +0900

    product data 수정

--- study_spring/jpa/src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
+++ study_spring/jpa/src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
@@ -1,4 +1,41 @@
 package com.springboot.jpa.data.dao.impl;
 
-public class ProductDAOImpl {
+import com.springboot.jpa.data.dao.ProductDAO;
+import com.springboot.jpa.data.entity.Product;
+import com.springboot.jpa.data.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductDAOImpl implements ProductDAO {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductDAOImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    @Override
+    public Product insertProduct(Product product) {
+        Product savedProduct = productRepository.save(product);
+
+        return savedProduct;
+    }
+
+    @Override
+    public Product selectProduct(Long number) {
+        Product selectedProduct = productRepository.getByID(number);
+    }
+
+    @Override
+    public Product updateProductName(Long number, String name) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception {
+
+    }
 }
